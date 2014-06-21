c5d94b330707179c6e5b309d911308d48dc2d06a
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-06-21 23:00:19 +0300

    make amazing angular menu

--- js/controllers/MenuController.js
+++ js/controllers/MenuController.js
@@ -9,14 +9,26 @@ define(['App'], function(App) {
 			value: '',
 			param: 'artist',
 			activeTag: '',
-			festivalsonly: 0
+			festivalsOnly: false
 		};
+
+		$scope.menu.tabs = ["artist", "city"];
 
 		$scope.switchTab = function(tab) {
 			$scope.menu.activeTab = tab;
 		};
 
 		$scope.menu.tags = ["rock", "pop", "alternative", "indie", "electronic", "classic rock", "hip-hop", "dance", "jazz"];
+
+		$scope.switchTag = function(tag) {
+			$scope.menu.activeTag = $scope.menu.activeTag == tag ? '' : tag;
+		};
+
+		$scope.menu.autocompleteItems = [];
+
+		$scope.setFestivalsOnly = function() {
+			$scope.menu.festivalsOnly = $scope.menu.festivalsOnly == false ? true : false;
+		};
 
 		$scope.years = ['2014', '2015', '2016', '2017'];
 
