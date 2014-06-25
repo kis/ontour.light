4c0d28a2fd8f844dcef92e51cfbf70b4863c0c24
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-06-25 23:41:32 +0300

    resolve model validating on blur

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ require.config({
 	},
 
 	paths: {
-		angular		    : 'lib/angular.min',
-		mapbox 		    : 'https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox',
-		cluster 	    : 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster',
-		scrollbar       : 'lib/scrollbar.min',
-		mousewheel      : 'lib/jquery.mousewheel'
+		angular		: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.13/angular.min',
+		mapbox 		: 'https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox',
+		cluster 	: 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster',
+		scrollbar   : 'lib/scrollbar.min',
+		mousewheel  : 'lib/jquery.mousewheel'
 	}
 });
 
