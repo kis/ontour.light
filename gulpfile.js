fc9cdade42b4455d9f637cf3145c9d7ab936a68f
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-09-02 10:10:16 +0300

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	minifyCss = require('gulp-minify-css'),
+	rename = require('gulp-rename');
+
+gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
-	gulp.src('assets/sass/*.scss')
+	gulp.src('assets/sass/styles.scss')
 	    .pipe(sass())
+	    .pipe(gulp.dest('css'))
+	    .pipe(minifyCss({
+	      keepSpecialComments: 0
+	    }))
+	    .pipe(rename({ extname: '.min.css' }))
 	    .pipe(gulp.dest('css'));
 });
