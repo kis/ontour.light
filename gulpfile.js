c69de4160e11f1f577951f26f23ffda60ce8db6a
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-09-11 11:30:03 +0300

    turn ejs to jade. clean code

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,15 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	minifyCss = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	jade = require('gulp-jade'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer-core'),
 	concat = require('gulp-concat');
 
 var paths = {
-	sass: ['public/assets/sass/*.scss'],
-	jade: ['public/assets/jade/index.jade']
+	sass: ['public/assets/sass/*.scss']
 };
 
-gulp.task('default', ['sass', 'jade']);
+gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
 	gulp.src(paths.sass)
@@ -27,13 +25,6 @@ gulp.task('sass', function () {
 	    .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('jade', function() {
-	gulp.src('public/assets/jade/index.jade')
-    	.pipe(jade())
-    	.pipe(gulp.dest('public'))
-});
-
 gulp.task('watch', function() {
 	gulp.watch(paths.sass, ['sass']);
-	gulp.watch(paths.jade, ['jade']);
 });
