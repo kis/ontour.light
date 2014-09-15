155469e0b16c7976be8f49b9ea7aa201f44eff6e
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-09-15 22:11:26 +0300

    menu

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,4 +9,12 @@ router.get('/welcome', function(req, res) {
   	res.render('welcome', { title: 'Express' });
 });
 
+router.get('/signin', function(req, res) {
+  	res.render('signin', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res) {
+  	res.render('signup', { title: 'Express' });
+});
+
 module.exports = router;
