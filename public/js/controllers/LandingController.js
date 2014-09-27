090e66df99d5481b8c5f0c123faefb8429365519
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-09-27 20:34:52 +0300

    Fix signup and signip form validation

--- public/js/controllers/LandingController.js
+++ public/js/controllers/LandingController.js
@@ -2,17 +2,26 @@ var ontour = angular.module('ontour', []);
 
 ontour.controller('LandingController', ['$scope', function ($scope) {
 	
+	$scope.user = {};
+
 	$scope.submitRegistration = function() {
-		if ($scope.regform.email.$dirty || $scope.regform.email.$error.required) {
-			console.log($scope.regform.email);
-		}
+		console.log($scope.user);
 	};
 
-	$scope.submitForm = function() {
+	$scope.submitLogin = function() {
+		console.log($scope.user);
+	};
+
+	$scope.signup = function() {
 		$scope.regform.email.submitted = true;
 		$scope.regform.login.submitted = true;
 		$scope.regform.password.submitted = true;
 		$scope.regform.password_repeat.submitted = true;
+	};
+
+	$scope.signin = function() {
+		$scope.loginform.login.submitted = true;
+		$scope.loginform.password.submitted = true;
 	};
 
 }]);
