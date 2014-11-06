61b8f8224cc4aa5e60dff2499f0d6974b0689af7
Kirill Stepkin <kirillstyopkin@gmail.com>
2014-11-06 16:55:56 +0200

    fix link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 9000,
     mongoose = require('mongoose'),
     passport = require('passport'),
     flash = require('connect-flash'),
